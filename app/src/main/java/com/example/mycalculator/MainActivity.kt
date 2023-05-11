41d7739abcc3899b0fd1f71d6768e7a3942a2d14
YasserBakkar <yasserbakkar97@gmail.com>
2023-05-11 18:54:10 +0300

    implementing CLR & dot

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,15 +3,32 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun onClick(view: View){
-        Toast.makeText(this, "Button Pressed" , Toast.LENGTH_SHORT).show()
+    fun onDigit(view: View){
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+    }
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
+    fun onDecimalPoint(view: View){
+        if (lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
